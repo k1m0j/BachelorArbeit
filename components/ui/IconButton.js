c1111b661a5c50ff6c20e05bd806eedb639b092f
Karl Schulz <kimoji@posteo.de>
2023-01-19 18:33:00 +0100

    Fix Icon Button pressed animation

--- components/ui/IconButton.js
+++ components/ui/IconButton.js
@@ -4,7 +4,7 @@ import { Ionicons } from "@expo/vector-icons";
 function IconButton({ icon, size, color, onPress }) {
   return (
     <Pressable
-      style={({ pressed }) => [styles.pressed, pressed && styles.pressed]}
+      style={({ pressed }) => [styles.buttonStyle, pressed && styles.pressed]}
       onPress={onPress}
     >
       <Ionicons name={icon} size={size} color={color}></Ionicons>
@@ -16,7 +16,6 @@ export default IconButton;
 
 const styles = StyleSheet.create({
   buttonStyle: {
-    padding: 8,
     justifyContent: "center",
     alignItems: "center",
   },
