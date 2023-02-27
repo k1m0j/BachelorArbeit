736174cfffb6877158780ca9292473aafca95806
Karl Schulz <kimoji@posteo.de>
2023-02-27 19:13:56 +0100

    Add bachelorthesis

--- constants/constants.js
+++ constants/constants.js
@@ -1,6 +1,4 @@
-export const GOOGLE_API_KEY = "AIzaSyAIRyLoE_yqsoJ2kR-fPEMma7IVT22svbY";
-
-export const OPENWEATHERMAP_API_KEY = "ce7df20b1e2dea8d24400dd966bc7973";
+export const GOOGLE_API_KEY = "GOOGLE_API_KEY";
 
 export const minWaypointDistance = 0.02;
 
